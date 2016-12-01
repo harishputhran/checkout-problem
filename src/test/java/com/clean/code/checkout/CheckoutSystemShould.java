17d11cea667a5d42fb0ae6b2b1b1f7f42b6c0490
Puthran, Harish <Harish.Puthran@in.tesco.com>
2016-12-01 09:03:34 +0000

    RED - test scenario for 1 Unit of Item A at checkout

---
+++ src/test/java/com/clean/code/checkout/CheckoutSystemShould.java
@@ -0,0 +1,16 @@
+package com.clean.code.checkout;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class CheckoutSystemShould {
+	
+	@Test
+	public void return_50_for_one_unit_of_itemA_at_checkout(){
+		CheckoutSystem checkoutSystem = new CheckoutSystem();
+		assertEquals(50, checkoutSystem.calculateTotalPrice("A"));
+	}
+
+}
