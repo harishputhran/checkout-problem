5fc6fc142ff76bdaae1081235bf56b9603b323b4
Puthran, Harish <Harish.Puthran@in.tesco.com>
2016-12-01 09:25:55 +0000

    RED - Test Scenario for 1 Unit Each of ItemA and ItemB

--- src/test/java/com/clean/code/checkout/CheckoutSystemShould.java
+++ src/test/java/com/clean/code/checkout/CheckoutSystemShould.java
@@ -2,6 +2,9 @@ package com.clean.code.checkout;
 
 import static org.junit.Assert.*;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
@@ -19,6 +22,13 @@ public class CheckoutSystemShould {
 	public void return_30_for_one_unit_of_itemB_at_checkout(){
 		CheckoutSystem checkoutSystem = new CheckoutSystem();
 		assertEquals(30, checkoutSystem.calculateTotalPrice("B"));
+	}
+	
+	@Test
+	public void return_80_for_one_unit_each_of_itemA_and_itemB_at_checkout(){
+		CheckoutSystem checkoutSystem = new CheckoutSystem();
+		List<String> itemsAtCheckout = Arrays.asList("A", "B");
+		assertEquals(80, checkoutSystem.calculateTotalPrice(itemsAtCheckout));
 	}
 
 }
